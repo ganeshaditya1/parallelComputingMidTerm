39dc9bc8b015d047add00d5ca44b7620acbef792
ganeshaditya1 <ganeshaditya1@gmail.com>
2016-11-21 10:06:51 -0500

    Working code

--- solver.c
+++ solver.c
@@ -12,7 +12,7 @@
 
 #define N 5000
 
-double ** A, **B, **C;
+double ** A, **B;
 int rank, size;
 MPI_Status Stat;
 
@@ -30,26 +30,40 @@ int initialize (double **A, int n)
 
 }
 
+
+
+void matrixPrint(double **A)
+{
+    for(int i = 0; i < 20 + 1; i++)
+    {
+        for(int j = 0; j < 20 + 1; j++)
+        {
+            printf(" %2.2f ", A[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    printf("\n\n");
+
+}
+
+
 void parallelSolve(double **A, int n)
 {
     
-    double start = MPI_Wtime();
+    
     int chunckSize = ceil((float)n/size);
     double message[N + 1], message2[N + 1], message3[N + 1], message4[N + 1];
-  
-    /*
-    
-    */
     int convergence=FALSE;
     double diff, tmp;
     int i,j, iters=0;
     int for_iters;
     MPI_Request reqs[4];
     MPI_Status stats[4];
+    int iter_bound = 21;
 
-    int jLower = rank * chunckSize + 1, jUpper = rank * chunckSize + 1;
-    
-   for (for_iters = 1; for_iters < 21; for_iters++) 
+    int jLower = (rank * chunckSize) + 1, jUpper = ((rank + 1) * chunckSize) + 1;
+   for (for_iters = 1; for_iters < iter_bound; for_iters++) 
    { 
         diff = 0.0;
         //No need to recieve any ghost regions for the first iteration
@@ -62,9 +76,10 @@ void parallelSolve(double **A, int n)
                 MPI_Recv(message, N + 1, MPI_DOUBLE, rank - 1,  0, MPI_COMM_WORLD, &Stat);
                 // Now copy the shadow region into it's proper place.
 
-                #pragma omp parallel for ordered schedule(static) num_threads(4)
+                #pragma omp parallel for ordered schedule(static) num_threads(2)
                 for(i = 1; i < n; i++)
                 {
+                    A[i][jLower - 1] =  A[i][jLower];
                     A[i][jLower] = message[1 + i];
                 }
             }
@@ -75,35 +90,28 @@ void parallelSolve(double **A, int n)
             {
                 MPI_Recv(message2, N + 1, MPI_DOUBLE, rank + 1,  0, MPI_COMM_WORLD, &Stat);
                 // Now copy the shadow region into it's proper place.
-                #pragma omp parallel for ordered schedule(static) num_threads(4)
-                for(i = 0; i < n; i++)
+                #pragma omp parallel for ordered schedule(static) num_threads(2)
+                for(i = 1; i < n; i++)
                 {
-                    A[i][jUpper] = message2[1 + i];
+                    A[i][jUpper] =  A[i][jUpper - 1];
+                    A[i][jUpper - 1] = message2[1 + i];
                 }
             }
         }
-        
-        /*#pragma omp parallel for ordered schedule(dynamic) num_threads(4)
          for (i=1;i<n;i++)
          {
-           for (j=1;j<n;j++)
+           for (j=jLower;j<jUpper;j++)
            {
              tmp = A[i][j];
              A[i][j] = 0.2*(A[i][j] + A[i][j-1] + A[i-1][j] + A[i][j+1] + A[i+1][j]);
              diff += fabs(A[i][j] - tmp);
            }
          }
-         iters++;
 
        
 
-         if (diff/((double)N*(double)N) < Tolerance)
-           convergence=TRUE;
-
-        }*/
-
         //No need to send any ghost regions for the last iteration
-        if(for_iters != 20)
+        if(for_iters != iter_bound - 1)
         {
 
             // Send to your left neighbour
@@ -111,10 +119,10 @@ void parallelSolve(double **A, int n)
             if(rank != 0)
             {               
                 // copy the shadow region to the message array.
-                #pragma omp parallel for ordered schedule(static) num_threads(4)
-                for(i = 0; i < n; i++)
+                #pragma omp parallel for ordered schedule(static) num_threads(2)
+                for(i = 1; i < n; i++)
                 {
-                    message3[1 + i] = 2 * i;
+                    message3[1 + i] = A[i][jLower];
                 }
                 MPI_Isend(message3, N + 1, MPI_DOUBLE, rank - 1,  0, MPI_COMM_WORLD, &reqs[1]);
             }
@@ -122,36 +130,18 @@ void parallelSolve(double **A, int n)
             // Only do this if you have a left neighbour
             if(rank != (size - 1))
             {
-                #pragma omp parallel for ordered schedule(static) num_threads(4)
-                for(i = 0; i < n; i++)
+                #pragma omp parallel for ordered schedule(static) num_threads(2)
+                for(i = 1; i < n; i++)
                 {
-                    message4[1 + i] = i;
+                    message4[1 + i] = A[i][jUpper];
                 }
                 MPI_Isend(message4, N + 1, MPI_DOUBLE, rank + 1,  0, MPI_COMM_WORLD, &reqs[3]);
             }
         }
 
     }
-    
 
-printf("%.2f time elasped\n", MPI_Wtime() - start);
-    /* No need to do this anymore
-    if(rank != 0 && rank != (size - 1))
-    {
-        MPI_Waitall(4, reqs, stats);
-    }
-    else if(rank != 0)
-    {
-        MPI_Waitall(2, reqs, stats);
-    }
-    else
-    {
-        MPI_Waitall(2, &reqs[2], stats);
-    }
-    */
-
-    
-    double *message5 = (double *)malloc(n * chunckSize * sizeof(double));
+    double *message5 = (double *)malloc((n + 2) * chunckSize * sizeof(double));
     int f = n - 1;
     if(rank == 0)
     {
@@ -159,7 +149,7 @@ printf("%.2f time elasped\n", MPI_Wtime() - start);
         {
             int lowerBound = target * chunckSize, upperBound = target == (size - 1) ? n : (target + 1) * chunckSize;
             MPI_Recv(message5, n * chunckSize, MPI_DOUBLE, target, 0, MPI_COMM_WORLD, &Stat);
-            #pragma omp parallel for ordered schedule(static) num_threads(4)
+            #pragma omp parallel for ordered schedule(static) num_threads(2)
             for(int i = 1; i < n; i++)
             {
                 for(int j = lowerBound; j < upperBound; j++)
@@ -176,7 +166,7 @@ printf("%.2f time elasped\n", MPI_Wtime() - start);
         
         int lowerBound = rank * chunckSize, upperBound = rank == (size - 1) ? n : (rank + 1) * chunckSize;
         //printf("%d %d \n", upperBound, lowerBound);
-        #pragma omp parallel for ordered schedule(static) num_threads(4)
+        #pragma omp parallel for ordered schedule(static) num_threads(2)
         for(int i = 1; i < n; i++)
         {
             for(int j = lowerBound; j < upperBound; j++)
@@ -197,7 +187,7 @@ void solve(double **A, int n)
    int for_iters;
 
 
-   for (for_iters=1;for_iters<21;for_iters++) 
+   for (for_iters=1;for_iters < 21;for_iters++) 
    { 
      diff = 0.0;
 
@@ -213,29 +203,31 @@ void solve(double **A, int n)
      iters++;
 
      if (diff/((double)N*(double)N) < Tolerance)
+    {
        convergence=TRUE;
+       printf("Number of iterations needed: %d\n", iters);
+       return;
+    }
 
     } /*for*/
 }
 
-int validator(double **A, double **B, int n)
+double validator(double **A, double **B, int n)
 {
+    double diff = 0, total = 0;
     for(int i = 0; i < n + 2; i++)
     {
         for(int j = 0; j < n + 2; j++)
         {
             if(A[i][j] != B[i][j])
             {
-                printf("%d %d\n", i, j);
+                printf("%d %d", i, j);
                 return 0;
             }
         }    
     }
-
     return 1;
-
 }
-
 
 long usecs (void)
 {
@@ -258,47 +250,42 @@ int main(int argc, char * argv[])
 
    A = malloc((N+2) * sizeof(double *));
    for (i=0; i<N+2; i++) {
-	   A[i] = malloc((N+2) * sizeof(double)); 
+	   A[i] = calloc((N+2), sizeof(double)); 
    }
 
    B = malloc((N+2) * sizeof(double *));
    for (i=0; i<N+2; i++) {
-       B[i] = malloc((N+2) * sizeof(double)); 
+       B[i] = calloc((N+2), sizeof(double)); 
    }
 
-   C = malloc((N+2) * sizeof(double *));
-   for (i=0; i<N+2; i++) {
-       C[i] = malloc((N+2) * sizeof(double)); 
-   }
    if(rank == 0)
    {
         initialize(A, N);
 
         t_start = MPI_Wtime();
-        //solve(A, N);
+        solve(A, N);
         t_end = MPI_Wtime();
 
 
         timeSerial = t_end - t_start;
         printf("Serial computation time = %f\n", timeSerial);
+        matrixPrint(A);
     }
    initialize(B, N);
-   initialize(C, N);
 
    t_start = MPI_Wtime();
    parallelSolve(B, N);
    t_end = MPI_Wtime();
 
-   if(rank == 0 && !validator(A, B, N))
-   {
-        fprintf(stderr, "Outputs don't match!\n");
-   }
+   
 
 	if(rank == 0)
     {
         timeParallel = t_end - t_start;
         printf("Parallel computation time = %f\n", timeParallel);    
         printf("Speedup is: [%.2fx]\n", timeSerial/timeParallel);
+        printf("Difference is %.2f\n", validator(A, B, N));
+        matrixPrint(B);
     }
 
 }
