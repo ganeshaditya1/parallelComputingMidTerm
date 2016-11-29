1ef245214965a7134917a572f9f7d8351541059e
ganeshaditya1 <ganeshaditya1@gmail.com>
2016-11-29 13:53:47 -0500

    asdf

--- ass4/helloWorld.cu
+++ ass4/helloWorld.cu
@@ -8,17 +8,17 @@ __global__ void kernel(int* a, int* b, int* c)
 
 int main()
 {
-	int *a, *b, *c;
+	int a, b, c;
 	int *d_a, *d_b, *d_c;
-	*a = 7;
-	*b = 8;
+	a = 7;
+	b = 8;
 	cudaMalloc((void **)&d_a, sizeof(int));
 	cudaMalloc((void **)&d_b, sizeof(int));
 	cudaMalloc((void **)&d_c, sizeof(int));
-	cudaMemcpy(d_a, a, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b, b, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
 	kernel<<<1, 1>>>(d_a, d_b, d_c);
-	cudaMemcpy(c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
-	printf("Asd %d\n", *c);
+	cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("Asd %d\n", c);
 	return 0;
 }
